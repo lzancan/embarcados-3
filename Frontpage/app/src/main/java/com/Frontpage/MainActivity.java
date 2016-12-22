cc154b149460f03515ba007e10e1b6b9afe2efcb
Luciano Zancan <luciano-zancan@hotmail.com>
2016-12-22 09:27:24 -0200

    Retirado botões, adicionado menu funcional no lugar dos botões...

--- Frontpage/app/src/main/java/com/Frontpage/MainActivity.java
+++ Frontpage/app/src/main/java/com/Frontpage/MainActivity.java
@@ -2,6 +2,8 @@ package com.Frontpage;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -31,16 +33,19 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MainActivity extends Activity {
 
-	private static final int RESULT_SETTINGS = 1;
-    /** Called when the activity is first created. */
-	public Button botaoinformar;
-	public Button botaosetarsites;
-	public TextView textviewcabecalho;
+    private static final int RESULT_SETTINGS = 1;
+    /**
+     * Called when the activity is first created.
+     */
+    public Button botaoinformar;
+    public Button botaosetarsites;
+    public TextView textviewcabecalho;
     private String ImageURL;
-	public ArrayList<Item> arrayitemlist = new ArrayList<Item>();
-	//public SettingsActivity settings;
+    public ArrayList<Item> arrayitemlist = new ArrayList<Item>();
+    //public SettingsActivity settings;
     public String URL = "http://g1.globo.com/dynamo/rs/rio-grande-do-sul/rss2.xml";
     public int maxQuant = 40;
+    private ListView listView=null;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
@@ -52,7 +57,7 @@ public class MainActivity extends Activity {
         textviewcabecalho.setText("Notícias");
 
         //click da listview
-        final ListView listView = (ListView) findViewById(R.id.ListView01);
+        listView = (ListView) findViewById(R.id.ListView01);
         listView.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
@@ -71,49 +76,12 @@ public class MainActivity extends Activity {
         // abre a janela das notícias
         OpenNews(URL, listView, maxQuant);
 
-        botaosetarsites = (Button) findViewById(R.id.Button02);
-        botaosetarsites.setText("Trocar Assunto");
-		botaosetarsites.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
-				startActivityForResult(i, RESULT_SETTINGS);
-			}
-		});
-
-        //pega id do botao
-        botaoinformar = (Button) findViewById(R.id.Button01);
-        //muda texto do botao
-        botaoinformar.setText("Informar Contato");
-        //clicklistener do botao
-        botaoinformar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                botaoinformar.setText("Selecione Link");
-                listView.setOnItemClickListener(new OnItemClickListener() {
-
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position,
-                                            long id) {
-
-                        Item item = arrayitemlist.get(position);
-                        String sendurl = item.getUrl();
-
-                        Intent sendIntent = new Intent();
-                        sendIntent.setAction(Intent.ACTION_SEND);
-                        sendIntent.putExtra(Intent.EXTRA_TEXT, sendurl);
-                        sendIntent.setType("text/plain");
-                        startActivity(sendIntent);
-                    }
-                });
-            }
-        });
 
     }
 
     // abre rss de notícias
     @TargetApi(9)
-    public void OpenNews(String Url,ListView listView, int maxQuant) {
+    public void OpenNews(String Url, ListView listView, int maxQuant) {
         try {
             // document builder (parser)
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -127,7 +95,7 @@ public class MainActivity extends Activity {
 
             String[] arrayTitles = new String[maxQuant];
             String[] arrayDetails = new String[maxQuant];
-            String[] arrayImages = new String [maxQuant];
+            String[] arrayImages = new String[maxQuant];
 
             if (listItem.getLength() > 0)
                 arrayitemlist.clear();
@@ -135,7 +103,7 @@ public class MainActivity extends Activity {
          /*   if (maxQuant <= 0)
                 null;//maxQuant = listItem.getLength();*/
             listView.setBackgroundColor(Color.LTGRAY);
-           // System.out.println(maxQuant);
+            // System.out.println(maxQuant);
             for (int x = 0; x < maxQuant; x++) {
                 //titulo
                 String title = listItem.item(x).getChildNodes().item(0).getChildNodes().item(0).getNodeValue();
@@ -144,19 +112,18 @@ public class MainActivity extends Activity {
                 String link = listItem.item(x).getChildNodes().item(1).getChildNodes().item(0).getNodeValue();
 
                 // detalhes
-                String details=listItem.item(x).getChildNodes().item(2).getChildNodes().item(0).getNodeValue();
+                String details = listItem.item(x).getChildNodes().item(2).getChildNodes().item(0).getNodeValue();
 
 
-                if (!details.startsWith("<")){
-                   ImageURL="";
-                }
-                else{
+                if (!details.startsWith("<")) {
+                    ImageURL = "";
+                } else {
                     // trata para pegar só a description
                     // pega imagem também
                     ImageURL = details.substring(details.lastIndexOf("src='") + 5);
-                    ImageURL=ImageURL.split("'")[0];
+                    ImageURL = ImageURL.split("'")[0];
                     //System.out.println(ImageURL);
-                    details=details.substring(details.lastIndexOf("br />") + 5); // avança 5 para começar após ">"
+                    details = details.substring(details.lastIndexOf("br />") + 5); // avança 5 para começar após ">"
                 }
                 //System.out.println(details);
                 Item item = new Item();
@@ -175,7 +142,7 @@ public class MainActivity extends Activity {
 
 
             listView.setAdapter(
-                    new LazyAdapter(this,arrayTitles,arrayDetails,arrayImages)
+                    new LazyAdapter(this, arrayTitles, arrayDetails, arrayImages)
             );
 
             textviewcabecalho.setText("Notícias de " + SourceName);
@@ -187,74 +154,74 @@ public class MainActivity extends Activity {
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RESULT_SETTINGS:
+                showUserSettings();
+                break;
 
-		switch (requestCode) {
-			case RESULT_SETTINGS:
-				showUserSettings();
-				break;
 
-		}
+        }
 
-	}
+    }
 
-	private void showUserSettings() {
-		SharedPreferences sharedPrefs = PreferenceManager
-				.getDefaultSharedPreferences(this);
+
+    private void showUserSettings() {
+        SharedPreferences sharedPrefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
 
         Editor editor = sharedPrefs.edit();
 
-		StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
 
-		TextView settingsTextView = (TextView) findViewById(R.id.TextView01);
+        TextView settingsTextView = (TextView) findViewById(R.id.TextView01);
 
-		settingsTextView.setText(builder.toString());
+        settingsTextView.setText(builder.toString());
 
         if (sharedPrefs.getString("pref", null) != null)
             URL = findURL(sharedPrefs.getString("pref", null));
         if (sharedPrefs.getString("num", null) != null)
-                maxQuant = Integer.parseInt(sharedPrefs.getString("num",null));
-                if(maxQuant<0)
-                    maxQuant=0;
-                if(maxQuant>40)
-                    maxQuant=40;
-                System.out.println (maxQuant);
-	}
+            maxQuant = Integer.parseInt(sharedPrefs.getString("num", null));
+        if (maxQuant < 0)
+            maxQuant = 0;
+        if (maxQuant > 40)
+            maxQuant = 40;
+        System.out.println(maxQuant);
+    }
 
 
+    @Override
+    public void onResume() {
+        super.onResume();
 
-	@Override
-	public void onResume() {
-		super.onResume();
+        //click da listview
+        final ListView listView = (ListView) findViewById(R.id.ListView01);
+        listView.setOnItemClickListener(new OnItemClickListener() {
 
-		//click da listview
-		final ListView listView = (ListView) findViewById(R.id.ListView01);
-		listView.setOnItemClickListener(new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position,
-									long id) {
+                Item item = arrayitemlist.get(position);
 
-				Item item = arrayitemlist.get(position);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
 
-				Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(item.getUrl()));
 
-				intent.setData(Uri.parse(item.getUrl()));
-
-				startActivity(intent);
-			}
-		});
-		botaoinformar.setText("Informar Contato");
+                startActivity(intent);
+            }
+        });
         OpenNews(URL, listView, maxQuant);
-	}
-@TargetApi(8)
-    private String findURL(String Url){
-    if(Url.toLowerCase().contains("http"))
-        return Url;
-    return "http://g1.globo.com/dynamo/".concat(Url).concat("/rss2.xml\"");
+    }
+
+    @TargetApi(8)
+    private String findURL(String Url) {
+        if (Url.toLowerCase().contains("http"))
+            return Url;
+        return "http://g1.globo.com/dynamo/".concat(Url).concat("/rss2.xml\"");
     }
 
     @Override
@@ -266,15 +233,74 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                OpenConfig();
+                return true;
+            case R.id.action_share:
+                OpenShare();
+                return true;
+            case R.id.action_help:
+                OpenHelp();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+    }
+
+    private void OpenConfig () {
+        Intent i = new Intent(getApplicationContext(), SettingsActivity.class);
+        startActivityForResult(i, RESULT_SETTINGS);
+    }
+    private void OpenShare () {
+        textviewcabecalho = (TextView) findViewById(R.id.TextView01);
+        textviewcabecalho.setText("Selecione a Notícia a ser Compartilhada");
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+
+                Item item = arrayitemlist.get(position);
+                String sendurl = item.getUrl();
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, sendurl);
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+    }
+    private void OpenHelp (){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                this);
+
+        // set title
+        alertDialogBuilder.setTitle("Sobre");
+
+        // set dialog message
+        alertDialogBuilder
+                .setMessage("CREATOR:\nLuciano Zancan\nE-MAIL:\nluciano-zancan@hotmail.com")
+                .setCancelable(false)
+                /*.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // if this button is clicked, close
+                        // current activity
+                        MainActivity.this.finish();
+                    }
+                })*/
+                .setNegativeButton("OK",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // if this button is clicked, just close
+                        // the dialog box and do nothing
+                        dialog.cancel();
+                    }
+                });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        // show it
+        alertDialog.show();
     }
 }
